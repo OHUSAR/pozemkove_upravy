8f0ed7f94a2651b1b50cea23abaf64da819a0b5b
Ondrej Husar <ondrej@husar.sk>
2016-04-24 14:17:39 +0200

    extract footer to separate file

--- sources/blog.php
+++ sources/blog.php
@@ -89,15 +89,7 @@
     </div>
     <!-- /.container -->
 
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php include('html/footer.html') ?>
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
