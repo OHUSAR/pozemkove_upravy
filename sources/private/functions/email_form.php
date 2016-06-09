e5332632926d6531224ced3facb1d59aebe4d41f
filipKov <filipKov@users.noreply.github.com>
2016-06-10 01:35:07 +0200

    Added Q&A section to private section, changed text in private section's index.php

---
+++ sources/private/functions/email_form.php
@@ -0,0 +1,19 @@
+<?php
+    function checkCorrectEmail($emailStr) {
+        return filter_var($emailStr, FILTER_VALIDATE_EMAIL);
+    }
+
+    function checkCorrectName($nameStr) {
+        return preg_match("/[^0-9]+/", $nameStr);
+    }
+
+    function checkContent($contentStr) {
+        return strlen($contentStr) >= 3;
+    }
+
+    function sendEmailWithQuestion ($name, $returnMail, $content) {
+
+        $mailContent = "Odosieľateľ: " . $name . "\nE-mail: " . $returnMail . "\nObsah otázky: " . $content;
+        $mailContent = wordwrap($mailContent, 70);
+        mail("filo.kovac@gmail.com", "Test mailov", $mailContent);
+    }
--- sources/private/index.php
+++ sources/private/index.php
@@ -20,9 +20,19 @@ if(! isset($_SESSION['name'])){
     <div class="row">
         <div class="box">
             <div class="col-lg-12">
-                <?php sectionHeading("Vitajte", "u nás")?>
+                <?php sectionHeading("Vítame Vás", "v osobnej časti")?>
                 <p>
-                    Ste prihlaseny v sukromej casti.
+                    Pripravili sme pre Vás ďalšie informácie a podklady nad rámec všeobecne dostupných informácií k pozemkovým úpravám, ktoré budú dostupné len Vám - účastníkom pozemkových úprav, na základe pridelených jedinečných prístupových údajov.
+                    <br><br>
+                    V prvej časti nájdete informácie o Vašich pozemkoch v obvode JPÚ ZB a DNV. V grafickej časti nájdete zobrazené Vaše pozemky v obvode pozemkových úprav, kde sú vyznačené sektory (projektové bloky) bližšie popísané v časti „o pozemkových úpravách“. V tabuľke nájdete sumárne základné informácie o Vašich pozemkoch (pozemok KN „E“ alebo „C“, druh pozemky, výmera, spoluvlastnícky podiel, výmera spoluvlastníckeho podielu).
+                    <br><br>
+                    V druhej časti môžete zadávať svoje otázky, dotazy, postrehy priamo prevádzkovateľovi tejto web stránky – spoločnosti Centrop s.r.o., v zastúpení Ing. Pavel Boroš.
+                    <br>
+                    Rovnako Vám budeme posielať upozornenia na nové informácie ohľadom pozemkových úprav prípadné ďalšie informácie alebo podklady.
+                    Veríme, že aj táto nadštandardná forma prispeje k zvýšeniu kvality procesu pozemkových úprav a umožní účastníkom dostávať čo najviac a najpodrobnejších informácií.
+                    <br><br>
+                    Ing. Pavel Boroš <br>
+                    Centrop s.r.o.
                 </p>
             </div>
         </div>
